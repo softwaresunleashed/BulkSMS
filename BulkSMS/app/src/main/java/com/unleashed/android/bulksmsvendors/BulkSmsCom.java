b3817499dcdae6014d77d0644e5eae477ee16ca2
Sudhanshu Gupta <sudhanshu.gupta@olx.com>
2016-09-05 18:35:09 +0530

    05 Sep 2016 -- Wrapper class to send bulk sms via site www.bulksms.com

---
+++ BulkSMS/app/src/main/java/com/unleashed/android/bulksmsvendors/BulkSmsCom.java
@@ -0,0 +1,57 @@
+package com.unleashed.android.bulksmsvendors;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+/**
+ * Created by Sudhanshu on 07-04-2016.
+ * Visit Site : http://developer.bulksms.com/eapi/code-samples/java/send_sms/
+ */
+public class BulkSmsCom {
+
+    public void SendMessage(String username, String password, String message){
+        try {
+            // Construct data
+            String data = "";
+            /*
+             * Note the suggested encoding for certain parameters, notably
+             * the username, password and especially the message.  ISO-8859-1
+             * is essentially the character set that we use for message bodies,
+             * with a few exceptions for e.g. Greek characters.  For a full list,
+             * see:  http://developer.bulksms.com/eapi/submission/character-encoding/
+             */
+            data += "username=" + URLEncoder.encode(username, "ISO-8859-1");
+            data += "&password=" + URLEncoder.encode(password, "ISO-8859-1");
+            data += "&message=" + URLEncoder.encode(message, "ISO-8859-1");
+            data += "&want_report=1";
+            data += "&msisdn=44123123123";
+
+            // Send data
+            // Please see the FAQ regarding HTTPS (port 443) and HTTP (port 80/5567)
+            URL url = new URL("https://bulksms.vsms.net/eapi/submission/send_sms/2/2.0");
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+
+            // Get the response
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                // Print the response output...
+                System.out.println(line);
+            }
+            wr.close();
+            rd.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
