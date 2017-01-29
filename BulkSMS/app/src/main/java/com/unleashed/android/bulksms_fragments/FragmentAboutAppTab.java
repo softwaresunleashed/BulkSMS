39d100d212fe4f66cfb76dbad3fac58fe9878c9e
Sudhanshu Gupta <softwares.unleashed@gmail.com>
2017-01-29 07:47:28 +0530

    27 Jan 2017 -- UI Modifications and few bug fixes

--- BulkSMS/app/src/main/java/com/unleashed/android/bulksms_fragments/FragmentAboutAppTab.java
+++ BulkSMS/app/src/main/java/com/unleashed/android/bulksms_fragments/FragmentAboutAppTab.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.daimajia.androidanimations.library.Techniques;
@@ -24,6 +25,7 @@ import com.unleashed.android.bulksms1.R;
 public class FragmentAboutAppTab extends PlaceholderFragment{
 
     // Handles to UI Controls on "About App -  Bulk SMS Tab"
+    private ImageView imageView;
     private TextView lbl_version_number;
     private TextView lbl_version_history;
     private TextView lbl_about_app;
@@ -46,9 +48,10 @@ public class FragmentAboutAppTab extends PlaceholderFragment{
         super.onResume();
 
         // Start Animations on opening About Tab
-        YoYo.with(Techniques.SlideInUp).duration(2000).playOn(lbl_version_number);
-        YoYo.with(Techniques.SlideInUp).duration(2000).playOn(lbl_version_history);
-        YoYo.with(Techniques.SlideInUp).duration(2000).playOn(lbl_about_app);
+        YoYo.with(Techniques.SlideInUp).duration(1500).playOn(imageView);
+        YoYo.with(Techniques.SlideInUp).duration(1500).playOn(lbl_version_number);
+        YoYo.with(Techniques.SlideInUp).duration(1500).playOn(lbl_version_history);
+        YoYo.with(Techniques.SlideInUp).duration(1500).playOn(lbl_about_app);
     }
 
     private void initAboutAppTab(View localView) {
@@ -70,6 +73,8 @@ public class FragmentAboutAppTab extends PlaceholderFragment{
         }
 
         AboutApp objAbtApp = new AboutApp(getActivity());
+
+        imageView = (ImageView) localView.findViewById(R.id.imageView);
 
         lbl_version_number = (TextView)localView.findViewById(R.id.lbl_app_version_number);
         lbl_version_number.setText(objAbtApp.getVersionNumber());
